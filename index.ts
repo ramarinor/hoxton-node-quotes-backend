f75a331f5d658cebe7c0a2a2cb3a2edc53da590d
Rinor Rama <rinor.rama95@gmail.com>
2022-02-15 19:21:04 +0100

    added delete and patch endpoints

--- index.ts
+++ index.ts
@@ -18,9 +18,9 @@ app.use(
     origin: "*"
   })
 );
-const quotes: Quote[] = [
+let quotes: Quote[] = [
   {
-    id: Math.random(),
+    id: 1,
     content: "To do or not to do!",
     firstName: "Nicolas",
     lastName: "Marcora",
@@ -29,7 +29,7 @@ const quotes: Quote[] = [
       "https://miro.medium.com/fit/c/1360/1360/1*GET26Y6wcljxmybUcxe0yw.jpeg"
   },
   {
-    id: Math.random(),
+    id: 2,
     content: "To math or not to math!",
     firstName: "Rinor",
     lastName: "Rama",
@@ -38,7 +38,7 @@ const quotes: Quote[] = [
       "https://scontent.fprn12-1.fna.fbcdn.net/v/t1.6435-9/106620065_10213860914317337_8468048895594297740_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=7OBp2vatqPAAX9jd86x&_nc_ht=scontent.fprn12-1.fna&oh=00_AT_MUHZtXNDU3EhitHEQTeP9VJTNAW3UpYI6hI3fDZzrnw&oe=62302D1D"
   },
   {
-    id: Math.random(),
+    id: 3,
     content: "To meth or not to meth!",
     firstName: "Random",
     lastName: "Drugster",
@@ -47,7 +47,7 @@ const quotes: Quote[] = [
       "https://static.toiimg.com/thumb/86802399.cms?width=680&height=512&imgsize=19342"
   },
   {
-    id: Math.random(),
+    id: 4,
     content: "To excercise or not to excercise!",
     firstName: "Elidon",
     lastName: "Morina",
@@ -55,7 +55,7 @@ const quotes: Quote[] = [
     image: "https://miro.medium.com/max/3150/0*DPy5UUHustmH8OKb."
   },
   {
-    id: Math.random(),
+    id: 5,
     content: "To ask or not to ask!",
     firstName: "Egon",
     lastName: "Loli",
@@ -63,7 +63,7 @@ const quotes: Quote[] = [
     image: "https://mapo.al/wp-content/uploads/2014/02/egon-loli-300x200.jpg"
   },
   {
-    id: Math.random(),
+    id: 6,
     content: "To help or not to help!",
     firstName: "Ed",
     lastName: "Putans",
@@ -72,7 +72,7 @@ const quotes: Quote[] = [
       "https://media-exp1.licdn.com/dms/image/C4D03AQEpcAcgGSoS_Q/profile-displayphoto-shrink_200_200/0/1633355002274?e=1648684800&v=beta&t=S3OxlNE-ELMcpM76gaNWsaMLnCZg40kpVbDocI3sxU0"
   },
   {
-    id: Math.random(),
+    id: 7,
     content: "To invade or not to invade!",
     firstName: "Vladimir",
     lastName: "Putin",
@@ -81,7 +81,7 @@ const quotes: Quote[] = [
       "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/Vladimir_Putin_%282020-02-20%29.jpg/1200px-Vladimir_Putin_%282020-02-20%29.jpg"
   },
   {
-    id: Math.random(),
+    id: 8,
     content: "To run or not to run!",
     firstName: "Ukrainian",
     lastName: "Soldier",
@@ -90,7 +90,7 @@ const quotes: Quote[] = [
       "https://www.vaticannews.va/content/dam/vaticannews/agenzie/images/afp/2021/04/27/14/1619525018556.jpg/_jcr_content/renditions/cq5dam.thumbnail.cropped.750.422.jpeg"
   },
   {
-    id: Math.random(),
+    id: 9,
     content: "To bomb or not to bomb!",
     firstName: "Joe",
     lastName: "Biden",
@@ -99,7 +99,7 @@ const quotes: Quote[] = [
       "https://www.whitehouse.gov/wp-content/uploads/2021/04/P20210303AS-1901-cropped.jpg"
   },
   {
-    id: Math.random(),
+    id: 10,
     content: "To gas or not to gas!",
     firstName: "Olaf",
     lastName: "Scholz",
@@ -108,6 +108,8 @@ const quotes: Quote[] = [
       "https://media.hotnews.ro/media_server1/image-2021-08-11-24972380-41-olaf-scholz.jpg"
   }
 ];
+
+let quoteIdCount = quotes.length;
 
 app.get("/", function (req, res) {
   res.send("We are learning node js!");
@@ -152,8 +154,9 @@ app.post("/quotes", (req, res) => {
   }
 
   if (errors.length === 0) {
+    quoteIdCount++;
     const newQuote: Quote = {
-      id: Math.random(),
+      id: quoteIdCount,
       content: content,
       firstName: firstName,
       lastName: lastName,
@@ -164,6 +167,39 @@ app.post("/quotes", (req, res) => {
     res.status(201).send(newQuote);
   } else {
     res.status(400).send({ errors: errors });
+  }
+});
+
+app.patch("/quotes/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const quoteToChange = quotes.find((quote) => quote.id === id);
+
+  if (quoteToChange) {
+    if (typeof req.body.content === "string")
+      quoteToChange.content = req.body.content;
+    if (typeof req.body.firstName === "string")
+      quoteToChange.firstName = req.body.firstName;
+    if (typeof req.body.lastName === "string")
+      quoteToChange.lastName = req.body.lastName;
+    if (typeof req.body.age === "number") quoteToChange.age = req.body.age;
+    if (typeof req.body.image === "string")
+      quoteToChange.image = req.body.image;
+    res.send(quoteToChange);
+  } else {
+    res.status(404).send({ error: "Dog not found." });
+  }
+});
+
+app.delete("/quotes/:id", (req, res) => {
+  const id = Number(req.params.id);
+
+  const match = quotes.find((quote) => quote.id === id);
+
+  if (match) {
+    quotes = quotes.filter((quote) => quote.id !== id);
+    res.send("Quote deleted sucessfully");
+  } else {
+    res.status(404).send({ error: "Quote not found" });
   }
 });
 
